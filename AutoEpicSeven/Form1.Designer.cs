d1d0ef62ea915a575196eca56145472a27b29fea
chien <chienpro987654@gmail.com>
2023-03-27 16:44:47 +0700

    init commit

---
+++ AutoEpicSeven/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AutoEpicSeven
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunBtn = new System.Windows.Forms.Button();
+            this.TestBtn = new System.Windows.Forms.Button();
+            this.CropBtn = new System.Windows.Forms.Button();
+            this.flagInput = new System.Windows.Forms.TextBox();
+            this.flagInputLabel = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.adbLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RunBtn
+            // 
+            this.RunBtn.Location = new System.Drawing.Point(292, 14);
+            this.RunBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RunBtn.Name = "RunBtn";
+            this.RunBtn.Size = new System.Drawing.Size(115, 46);
+            this.RunBtn.TabIndex = 4;
+            this.RunBtn.Text = "Run";
+            this.RunBtn.UseVisualStyleBackColor = true;
+            this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+            // 
+            // TestBtn
+            // 
+            this.TestBtn.Location = new System.Drawing.Point(24, 140);
+            this.TestBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TestBtn.Name = "TestBtn";
+            this.TestBtn.Size = new System.Drawing.Size(115, 46);
+            this.TestBtn.TabIndex = 5;
+            this.TestBtn.Text = "Test";
+            this.TestBtn.UseVisualStyleBackColor = true;
+            this.TestBtn.Click += new System.EventHandler(this.TestBtn_Click);
+            // 
+            // CropBtn
+            // 
+            this.CropBtn.Location = new System.Drawing.Point(24, 69);
+            this.CropBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CropBtn.Name = "CropBtn";
+            this.CropBtn.Size = new System.Drawing.Size(115, 46);
+            this.CropBtn.TabIndex = 6;
+            this.CropBtn.Text = "Crop";
+            this.CropBtn.UseVisualStyleBackColor = true;
+            this.CropBtn.Click += new System.EventHandler(this.CropBtn_Click);
+            // 
+            // flagInput
+            // 
+            this.flagInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flagInput.Location = new System.Drawing.Point(235, 20);
+            this.flagInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flagInput.Name = "flagInput";
+            this.flagInput.Size = new System.Drawing.Size(41, 30);
+            this.flagInput.TabIndex = 7;
+            // 
+            // flagInputLabel
+            // 
+            this.flagInputLabel.AutoSize = true;
+            this.flagInputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flagInputLabel.Location = new System.Drawing.Point(28, 27);
+            this.flagInputLabel.Name = "flagInputLabel";
+            this.flagInputLabel.Size = new System.Drawing.Size(185, 25);
+            this.flagInputLabel.TabIndex = 8;
+            this.flagInputLabel.Text = "Enter flag\'s number:";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel.Location = new System.Drawing.Point(288, 62);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(104, 20);
+            this.countLabel.TabIndex = 9;
+            this.countLabel.Text = "0 time(s) left";
+            // 
+            // adbLabel
+            // 
+            this.adbLabel.AutoSize = true;
+            this.adbLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adbLabel.Location = new System.Drawing.Point(183, 161);
+            this.adbLabel.Name = "adbLabel";
+            this.adbLabel.Size = new System.Drawing.Size(64, 25);
+            this.adbLabel.TabIndex = 10;
+            this.adbLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 214);
+            this.Controls.Add(this.adbLabel);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.flagInputLabel);
+            this.Controls.Add(this.flagInput);
+            this.Controls.Add(this.CropBtn);
+            this.Controls.Add(this.TestBtn);
+            this.Controls.Add(this.RunBtn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Auto EpicSeven";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button RunBtn;
+        private System.Windows.Forms.Button TestBtn;
+        private System.Windows.Forms.Button CropBtn;
+        private System.Windows.Forms.TextBox flagInput;
+        private System.Windows.Forms.Label flagInputLabel;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label adbLabel;
+    }
+}
+
---
+++ AutoEpicSeven/Form1.cs
@@ -0,0 +1,352 @@
+﻿using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using SixLabors.ImageSharp;
+using static System.Windows.Forms.LinkLabel;
+using System.Reflection.Emit;
+
+namespace AutoEpicSeven
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            flagInput.Text = "10";
+        }
+        private void runAdb()
+        {
+            //Cap screenshot from phone
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("adb devices");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            var counter = 0;
+            while (!cmd.StandardOutput.EndOfStream)
+            {
+                var line = cmd.StandardOutput.ReadLine();
+                counter++;
+
+                if (counter == 6 || counter == 8)
+                {
+                    if (line.Contains("device"))
+                    {
+                        adbLabel.Text = "adb starts successfully!";
+                        break;
+                    }
+                    
+                }
+            }
+        }
+
+        private void captureScreenshot()
+        {
+            //Cap screenshot from phone
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("adb exec-out screencap -p > D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\screenshot.png");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Debug.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        private string getFlag()
+        {
+            //Get number of flags in screenshot
+            Bitmap image = new Bitmap(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png");
+            Crop filter = new Crop(new System.Drawing.Rectangle(1120, 30, 93, 40));
+            // apply the filter
+            Bitmap newImage = filter.Apply(image);
+            newImage.Save(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\flag.png");
+            //location to testdata for eng.traineddata
+            var path = @"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\tessdata";
+            //your sample image location
+            var sourceFilePath = @"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\flag.png";
+            using (var engine = new TesseractEngine(path, "eng"))
+            {
+                engine.SetVariable("user_defined_dpi", "70"); //set dpi for supressing warning
+                using (var img = Pix.LoadFromFile(sourceFilePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        var text = page.GetText();
+                        return text;
+                    }
+                }
+            }
+        }
+
+        private void clickXY(int x1, int x2,int y1, int y2)
+        {
+            Random r = new Random();
+            int x = r.Next(x1, x2);
+
+            int y = r.Next(y1, y2);
+
+            //Debug.WriteLine(x1);
+            //Debug.WriteLine(x2);
+            //Debug.WriteLine(y1);
+            //Debug.WriteLine(y2);
+            //Debug.WriteLine(x);
+            //Debug.WriteLine(y);
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("adb shell input tap "+x.ToString()+" "+y.ToString());
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Debug.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        private int findRetry()
+        {
+            using (Bitmap myBitmap = new Bitmap(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png"))
+            {
+                int[] pixels = { 270, 435, 600, 765, 935 };
+
+                for (int i = 0; i <= 4; i++)
+                {
+                    System.Drawing.Color pixelColor = myBitmap.GetPixel(1635, pixels[i]);
+                    //Debug.WriteLine(pixelColor.ToString());
+                    if (pixelColor.R > 20)
+                        return pixels[i];
+                }
+                return 0;
+            }  
+        }
+
+        private Bitmap cropImage(Bitmap image, int x1, int x2, int y1, int y2)
+        {
+
+            Crop filter = new Crop(new System.Drawing.Rectangle(x1,x2,y1,y2));
+            // apply the filter
+            Bitmap newImage = filter.Apply(image);
+            System.Drawing.Rectangle rec = new System.Drawing.Rectangle(0,0,newImage.Width, newImage.Height);
+            Bitmap bitmap = newImage.Clone(rec, PixelFormat.Format24bppRgb);
+            
+            //Bitmap bp = new Bitmap(newImage.Width, newImage.Height, PixelFormat.Format24bppRgb);
+            //using (Graphics gr = Graphics.FromImage(bp))
+            //    gr.DrawImage(newImage, new System.Drawing.Rectangle(0, 0, bp.Width, bp.Height));
+            return bitmap;
+        }
+
+        private bool compareImage(Bitmap croppedSource, Bitmap template, int x, int y, int width, int height)
+        {
+            Crop filter = new Crop(new System.Drawing.Rectangle(x, y, width, height));
+            // apply the filter
+            Bitmap croppedTemplate = filter.Apply(template);
+
+            System.Drawing.Rectangle rec = new System.Drawing.Rectangle(0, 0, croppedSource.Width, croppedSource.Height);
+            Bitmap formatted_source = croppedSource.Clone(rec, PixelFormat.Format24bppRgb);
+            Bitmap formatted_template = croppedTemplate.Clone(rec, PixelFormat.Format24bppRgb);
+
+            formatted_source.Save(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\non_format_s.png");
+            formatted_template.Save(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\non_format_t.png");
+
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0);
+            // compare two images
+            TemplateMatch[] matchings = tm.ProcessImage(formatted_source, formatted_template);
+            // check similarity level
+            if (matchings[0].Similarity > 0.95f)
+            {
+                Debug.WriteLine("The Same");
+                return true;
+            }
+            else
+            {
+                Debug.WriteLine("Not the Same");
+            }
+            return false;
+        }
+
+        private void CropBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap image = AForge.Imaging.Image.FromFile(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png");
+            Crop filter = new Crop(new System.Drawing.Rectangle(760, 600, 650, 130));
+            // apply the filter
+            Bitmap newImage = filter.Apply(image);
+            newImage.Save(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\exit_760_600_650_130.png");
+        }
+
+        private void TestBtn_Click(object sender, EventArgs e)
+        {
+            runAdb();
+        }
+
+        //click retry
+        //click retry confirm
+        //click start battle
+        //click menu
+        //click yeild
+        //click exit
+        //click defeat confirm
+
+        //kiểm tra flag
+        //chạy theo thứ tự retry 3 lần
+        //nếu khớp thì chạy bth
+        //nếu ko khớp thì retry
+
+        //từ array screenshot
+        //so sánh 2 phần ảnh screenshot
+        //  giống: click điểm - đi tới so sánh tiếp theo
+        //  khác: lùi lại, so sánh lại cặp trước đó.
+
+
+        private void clickEvent()
+        {
+            String s1 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\retry_confirm_740_650_670_140.png";
+            String s2 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\start_1550_960_120_60.png";
+            String s3 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\fighting_2015_110_115_110.png";
+            String s4 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\yeild_860_355_440_115.png";
+            string s5 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\exit_760_600_650_130.png";
+            String s6 = "D:\\Hoc_tap\\C#\\AutoEpicSeven\\AutoEpicSeven\\images\\defeat_confirm_800_260_560_140.png";
+
+            String[] sources = { s1, s2, s3, s4, s5, s6 };
+            int[,] recs = new int[,] { { 740, 650, 670, 140 }, { 1550, 960, 120, 60 }, { 2015, 110, 115, 110 }, { 860, 355, 440, 115 }, { 760, 600, 650, 130 }, { 800, 260, 560, 140 } };
+
+            int[,] clicks = new int[,] { { 1155, 1320, 700, 740 }, { 1550, 1650, 970, 1010 }, { 2100, 2110, 35, 50 }, { 970, 1200, 400, 430 }, { 1165, 1310, 640, 680 }, { 1915, 2050, 970, 1010 } };
+
+            int[] sleepTime = { 7000, 1000, 1000, 1000 ,1000};
+
+            File.Delete(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png");
+            captureScreenshot();
+            Debug.WriteLine("Captured");
+            Thread.Sleep(1000);
+
+
+
+            for(int i =0;i<3; i++)
+            {
+                int x = findRetry();
+                clickXY(1600, 1675, x, x + 50); //Click Retry Button
+                Thread.Sleep(1000);
+                captureScreenshot();
+                Thread.Sleep(1000);
+                Bitmap croppedSource = new Bitmap(sources[0]);
+                using (Bitmap template = new Bitmap(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png"))
+                {
+                    if (compareImage(croppedSource, template, recs[0, 0], recs[0, 1], recs[0, 2], recs[0, 3]))
+                    {
+                        Thread.Sleep(1000);
+                        clickXY(clicks[0, 0], clicks[0, 1], clicks[0, 2], clicks[0, 3]); //Click button
+                        Thread.Sleep(3000);
+                        break;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Captured");
+                        clickXY(55, 70, 45, 60);
+                        Thread.Sleep(1000);
+                    }
+                }
+
+            }
+
+            int count = 3;
+
+            for (int i = 2; i <= 6; i++)
+            {
+                while (count != 0)
+                {
+                    Debug.WriteLine("Step" + i);
+                    if ( i>=2 && i<=6)
+                    {
+                        clickXY(clicks[i-1, 0], clicks[i-1, 1], clicks[i-1, 2], clicks[i-1, 3]); //Click button
+                    }
+                    Thread.Sleep(sleepTime[i - 2]);
+                    captureScreenshot();
+                    Debug.WriteLine("i="+i);
+                    Thread.Sleep(1000);
+                    if (i == 6) break;
+                    Bitmap croppedSource = new Bitmap(sources[i]);
+                    Debug.WriteLine(sources[i].Substring(49, 10));
+                    using (Bitmap template = new Bitmap(@"D:\Hoc_tap\C#\AutoEpicSeven\AutoEpicSeven\images\screenshot.png"))
+                    {
+                        if (!compareImage(croppedSource, template, recs[i, 0], recs[i, 1], recs[i, 2], recs[i, 3]))
+                        {
+                            //Thread.Sleep(1000);
+                            //clickXY(clicks[i, 0], clicks[i, 1], clicks[i, 2], clicks[i, 3]); //Click button
+                            //Thread.Sleep(2000);
+                            //break;
+                            if (i > 2 && i < 5)
+                            {
+                                i--;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    count--;
+                }
+                count = 3;
+            }
+        }
+
+        private void RunBtn_Click(object sender, EventArgs e)
+        {
+            captureScreenshot();
+            int flag = 0;
+            try
+            {
+                flag = int.Parse(flagInput.Text);
+                if (flag <=0) { throw new FormatException(); }
+            }
+            catch (FormatException)
+            {
+                string message = "Flag's number must be a positive number!";
+                string title = "Error";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            while (flag != 0)
+            {
+                countLabel.Text = flag.ToString() + " time(s) left";
+                clickEvent();
+                flag--;
+            }
+            string message1 = "Running complete!";
+            string title1 = "Notification";
+            MessageBox.Show(message1, title1);
+        }
+    }
+}
